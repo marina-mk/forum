2e78659da5dd4c8b7d8bd929b881109fbb689d9f
marina-mk <maryna.klimchuk@gmail.com>
2019-09-11 20:31:56 +0200

    React-Forum[33]: Encrypt passwords for User schema

--- models/User.js
+++ models/User.js
@@ -1,11 +1,28 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const { Schema } = mongoose;
+const saltRounds = 10;
 
 const userSchema = new Schema({
-    name: { type: String, required: true },
+    name: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+});
+
+userSchema.pre("save", function pre(next) {
+    if (this.isNew || this.isModified("password")) {
+        bcrypt.hash(this.password, saltRounds, (err, hashedPassword) => {
+            if (err) {
+                next(err);
+            } else {
+                this.password = hashedPassword;
+                next();
+            }
+        });
+    } else {
+        next();
+    }
 });
 
 mongoose.model("users", userSchema);
