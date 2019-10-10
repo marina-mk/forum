6a15df78a536b39fdd7fcba99d34d074b43ae7e4
marina-mk <maryna.klimchuk@gmail.com>
2019-10-10 17:18:57 +0300

    React-Forum[60]: Forum Breadcrumbs component logic

--- client/src/components/content/Breadcrumbs.js
+++ client/src/components/content/Breadcrumbs.js
@@ -1,20 +1,53 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import * as actions from '../../actions';
 
-const Breadcrumbs = ({ params }) => (
-  <nav className="breadcrumbs navbar navbar-dark bg-dark-nav-color">
-    <div className="navbar-nav">
-      <a className="nav-item nav-link" href="#">Section</a>
-      <a className="nav-item nav-link" href="#">Topic</a>
-    </div>
-  </nav>
-);
+const Breadcrumbs = ({
+  params, sections, topics, fetchSections, fetchTopics,
+}) => {
+  useEffect(() => {
+    if (!sections.length) {
+      fetchSections();
+    }
+
+    if (!topics.length) {
+      fetchTopics(params.section);
+    }
+  }, []);
+
+  const currentSection = sections.find((section) => section.name === params.section);
+  const currentTopic = topics.find((topic) => topic.index === +params.topic);
+
+  return (
+    <nav className="breadcrumbs navbar navbar-dark bg-dark-nav-color">
+      <div className="navbar-nav">
+        {currentSection && <Link to={`/${currentSection.name}`} className="nav-item nav-link">{currentSection.title}</Link>}
+        {currentTopic && <Link to={`/${currentTopic.index}`} className="nav-item nav-link">{currentTopic.title}</Link>}
+      </div>
+    </nav>
+  );
+};
 
 Breadcrumbs.propTypes = {
   params: PropTypes.shape({
     section: PropTypes.string.isRequired,
     topic: PropTypes.string,
   }).isRequired,
+  sections: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  })).isRequired,
+  topics: PropTypes.arrayOf(PropTypes.shape({
+    index: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+  })).isRequired,
+  fetchSections: PropTypes.func.isRequired,
+  fetchTopics: PropTypes.func.isRequired,
 };
 
-export default Breadcrumbs;
+const mapStateToProps = ({ sections, topics }) => ({ sections, topics });
+
+export default connect(mapStateToProps, actions)(Breadcrumbs);
