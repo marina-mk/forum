713666d18671a4b3bcf6970ea98b1bbb3c4e3628
marina-mk <maryna.klimchuk@gmail.com>
2019-10-15 21:26:19 +0300

    React-Forum[66]: Add error handling for actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,22 +1,39 @@
+/* eslint-disable no-console */
 import axios from 'axios';
 import * as types from './types';
 
 export const fetchUser = () => async (dispatch) => {
-  const response = await axios.get('/api/auth');
-  dispatch({ type: types.FETCH_USER_DATA, payload: response.data });
+  try {
+    const response = await axios.get('/api/auth');
+    dispatch({ type: types.FETCH_USER_DATA, payload: response.data });
+  } catch (error) {
+    console.log(error.message, error.response.data);
+  }
 };
 
 export const logoutUser = () => async (dispatch) => {
-  await axios.delete('/api/logout');
-  dispatch({ type: types.DELETE_USER_DATA });
+  try {
+    await axios.delete('/api/logout');
+    dispatch({ type: types.DELETE_USER_DATA });
+  } catch (error) {
+    console.log(error.message, error.response.data);
+  }
 };
 
 export const fetchSections = () => async (dispatch) => {
-  const response = await axios.get('/api/sections');
-  dispatch({ type: types.FETCH_SECTIONS_DATA, payload: response.data });
+  try {
+    const response = await axios.get('/api/sections');
+    dispatch({ type: types.FETCH_SECTIONS_DATA, payload: response.data });
+  } catch (error) {
+    console.log(error.message, error.response.data);
+  }
 };
 
 export const fetchTopics = (section) => async (dispatch) => {
-  const response = await axios.get(`/api/topics/${section}`);
-  dispatch({ type: types.FETCH_TOPICS_DATA, payload: response.data });
+  try {
+    const response = await axios.get(`/api/topics/${section}`);
+    dispatch({ type: types.FETCH_TOPICS_DATA, payload: response.data });
+  } catch (error) {
+    console.log(error.message, error.response.data);
+  }
 };
