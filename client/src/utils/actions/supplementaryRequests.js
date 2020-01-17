e25eca3d4252b36f9c2147c9aa0be7759cf1dde9
marina-mk <maryna.klimchuk@gmail.com>
2020-01-17 17:34:00 +0300

    React-Forum[108]: Fix supplementary requests' catch mechanism

--- client/src/utils/actions/supplementaryRequests.js
+++ client/src/utils/actions/supplementaryRequests.js
@@ -1,33 +1,33 @@
 /* eslint-disable no-empty */
 import axios from 'axios';
 
-export const updateSectionTopicsCount = (sectionId) => {
+export const updateSectionTopicsCount = async (sectionId) => {
   try {
-    axios.patch(`/api/sections/${sectionId}`, { topicsCount: true });
+    await axios.patch(`/api/sections/${sectionId}`, { topicsCount: true });
   } catch (err) {}
 };
 
-export const updateSectionPostsCount = (sectionId) => {
+export const updateSectionPostsCount = async (sectionId) => {
   try {
-    axios.patch(`/api/sections/${sectionId}`, { postsCount: true });
+    await axios.patch(`/api/sections/${sectionId}`, { postsCount: true });
   } catch (err) {}
 };
 
-export const updateTopicPostsCount = (sectionId, topicId) => {
+export const updateTopicPostsCount = async (sectionId, topicId) => {
   try {
-    axios.patch(`/api/sections/${sectionId}/topics/${topicId}`, { postsCount: true });
+    await axios.patch(`/api/sections/${sectionId}/topics/${topicId}`, { postsCount: true });
   } catch (err) {}
 };
 
-export const updateTopicViews = (sectionId, topicId) => {
+export const updateTopicViews = async (sectionId, topicId) => {
   try {
-    axios.patch(`/api/sections/${sectionId}/topics/${topicId}`, { views: true });
+    await axios.patch(`/api/sections/${sectionId}/topics/${topicId}`, { views: true });
   } catch (err) {}
 };
 
-export const updateUserTopicsCount = () => {
+export const updateUserTopicsCount = async () => {
   try {
-    axios.patch(`/api/user`, { topicsCount: true });
+    await axios.patch(`/api/user`, { topicsCount: true });
   } catch (err) {}
 };
 
